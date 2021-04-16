cbfeff290532e4da9455555922a02c0d405cd938
windo-developer <yui050505@gmail.com>
2021-04-16 00:16:59 -0700

    create navigation stack

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import LoggedOutNav from "./src/routes/LoggedOutNav";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => {
+    setLoading(false);
+  };
+  const preload = () => {
+    const fontsPreload = [Ionicons.font];
+    const fontPromises = fontsPreload.map((font) => Font.loadAsync(font));
+    const imagesPreload = [require("./assets/instagramlogo.png")];
+    const imagesPromises = imagesPreload.map((image) => Asset.loadAsync(image));
+    return Promise.all([...fontPromises, ...imagesPromises]);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        onError={console.warn}
+        onFinish={onFinish}
+        startAsync={preload}
+      />
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <LoggedOutNav />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,4 @@
+# React Native App (EXPO)
+
+backend - https://github.com/windo-developer/Prisma-backend
+frontend(Web) - https://github.com/windo-developer/SNS-frontend
